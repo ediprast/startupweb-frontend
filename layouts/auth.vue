2ef284ef6e053a63e3ded0f5d5fbc4f6a99c56b1
ediprast <ediprastyo96@gmail.com>
2022-08-09 13:48:26 +0700

    first commit

---
+++ layouts/auth.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="auth-page">
+        <Nuxt/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'auth'
+}
+</script>
---
+++ layouts/success.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="success-page">
+        <Nuxt/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'success'
+}
+</script>
